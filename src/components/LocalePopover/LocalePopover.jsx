5a0034f5e2241a417256d3927473d119baec0436
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2022-08-14 22:00:18 +0300

    finish tasks

--- src/components/LocalePopover/LocalePopover.jsx
+++ src/components/LocalePopover/LocalePopover.jsx
@@ -1,15 +1,10 @@
-import Select from '@mui/material/Select';
 import IconButton from '@mui/material/IconButton';
 import Popover from '@mui/material/Popover';
-
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Language from '@mui/icons-material/Language';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { changeLocale } from 'redux/actions';
 import { useState } from 'react';
-import i18next from 'i18next';
 
 export const LocalePopover = () => {
   const dispatch = useDispatch();
@@ -17,13 +12,11 @@ export const LocalePopover = () => {
 
   const handleChangeLocale = event => {
     dispatch(changeLocale(event.target.textContent === 'en' ? 'en' : 'uk'));
-    i18next.changeLanguage(event.target.dataset.lang);
     setAnchorEl(null);
   };
 
   const handleLocaleClick = event => {
     setAnchorEl(event.currentTarget);
-    // setIsPopoverOpen(prevState => !prevState);
   };
 
   return (
@@ -38,9 +31,6 @@ export const LocalePopover = () => {
         transformOrigin={{ vertical: 'top', horizontal: 'center' }}
         onClose={() => setAnchorEl(null)}
       >
-        {/* <MenuItem value="uk" onClick={handleChangeLocale}>
-          en
-        </MenuItem> */}
         <MenuItem data-lang="en" onClick={handleChangeLocale}>
           en
         </MenuItem>
